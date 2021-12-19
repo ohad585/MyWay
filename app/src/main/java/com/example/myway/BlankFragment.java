06c508ddd87781463c90e112a7edba58105d7ab6
ohad shalom <ohad5855@gmail.com>
2021-12-19 12:53:04 +0200

    home/personal page xml

--- app/src/main/java/com/example/myway/BlankFragment.java
+++ app/src/main/java/com/example/myway/BlankFragment.java
@@ -14,6 +14,6 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.log_in_fragment, container, false);
+        return inflater.inflate(R.layout.home_page_fragment, container, false);
     }
 }
