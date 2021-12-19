dcb99fea2c60e697e81169af5f1eec3047e3b42d
ohad shalom <ohad5855@gmail.com>
2021-12-19 11:42:40 +0200

    First commit

---
+++ app/src/main/java/com/example/myway/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myway;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    NavController navCtrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavHostFragment nav_host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.base_navhost);
+        navCtrl = nav_host.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navCtrl);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
