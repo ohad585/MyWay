5a4821a3de6f7cc783dd0d6419dda40bc1ce8c6f
ohad shalom <ohad5855@gmail.com>
2022-01-06 12:08:09 +0200

    google maps activity

--- app/src/main/java/com/example/myway/MainActivity.java
+++ app/src/main/java/com/example/myway/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this,navCtrl);
 
 
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/myway/MainActivity2.java
@@ -0,0 +1,72 @@
+package com.example.myway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import org.json.JSONException;
+
+public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Set the map coordinates to Sami shamoon ashdod.
+        LatLng samiShamoon = new LatLng(31.80687, 34.65846);
+        // Set the map type to Normal.
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // Add a marker on the map coordinates.
+        googleMap.addMarker(new MarkerOptions()
+                .position(samiShamoon)
+                .title("Sami"));
+        // Move the camera to the map coordinates and zoom in closer.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(samiShamoon));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+        Polygon polygon = googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.8073, 34.65781),//Top left
+                        new LatLng(31.80728, 34.65785),//Top right
+                        new LatLng(31.8072, 34.65779),//Botom right
+                        new LatLng(31.80722, 34.65775))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        polygon.setTag("Room 160");
+
+        // Display traffic.
+        googleMap.setTrafficEnabled(true);
+        googleMap.setOnPolygonClickListener(new GoogleMap.OnPolygonClickListener() {
+            public void onPolygonClick(Polygon polygon) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(googleMap.getCameraPosition().target, googleMap.getCameraPosition().zoom));
+                Log.d("TAG", "onPolygonClick: Clicked");
+
+
+            }
+        });
+    }
+
+
+
+}
