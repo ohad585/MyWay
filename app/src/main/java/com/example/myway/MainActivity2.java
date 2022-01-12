58c4287c5a4443bef731032790a651849d804d14
liron hamo <lironhamo8@gmail.com>
2022-01-12 09:51:26 +0200

    fix plygons 12.1.22

--- app/src/main/java/com/example/myway/MainActivity2.java
+++ app/src/main/java/com/example/myway/MainActivity2.java
@@ -54,125 +54,117 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
                 .fillColor(0x7F00FF00)
                 .clickable(true));
         room160.setTag("Room 160");
-        
-//        Polygon room162 = googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80721, 34.65787),//Top left
-//                        new LatLng(31.80719, 34.65793),//Top right
-//                        new LatLng(31.80714, 34.65789),//Botom right
-//                        new LatLng(31.80717, 34.65784))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room162.setTag("Room 162");
-//
-//        Polygon room163= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80719, 34.65793
-//                        ),//Top left
-//                        new LatLng(31.80716, 34.658
-//                        ),//Top right
-//                        new LatLng(31.80711, 34.65796
-//                        ),//Botom right
-//                        new LatLng(31.80714, 34.6579
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room163.setTag("Room 163");
-//
-//        Polygon room164= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80712, 34.65802
-//                        ),//Top left
-//                        new LatLng(31.8071, 34.65808
-//                        ),//Top right
-//                        new LatLng(31.80707, 34.65804
-//                        ),//Botom right
-//                        new LatLng(31.80709, 34.658
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room164.setTag("Room 164");
-//
-//        Polygon room165= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80715, 34.6581
-//                        ),//Top left
-//                        new LatLng(31.80712, 34.65816
-//                        ),//Top right
-//                        new LatLng(31.80709, 34.65814
-//                        ),//Botom right
-//                        new LatLng(31.80711, 34.65807
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room165.setTag("Room 165");
-//
-//        Polygon room166B= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80706, 34.65817
-//                        ),//Top left
-//                        new LatLng(31.80702, 34.65822
-//                        ),//Top right
-//                        new LatLng(31.80699, 34.65819
-//                        ),//Botom right
-//                        new LatLng(31.80702, 34.65814
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room166B.setTag("Room 166B");
-//
-//        Polygon room166C= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80701, 34.65826
-//                        ),//Top left
-//                        new LatLng(31.80699, 34.6583
-//                        ),//Top right
-//                        new LatLng(31.80695, 34.65828
-//                        ),//Botom right
-//                        new LatLng(31.80697, 34.65824
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room166C.setTag("Room 166C");
-//
-//        Polygon room166A= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80707, 34.65827
-//                        ),//Top left
-//                        new LatLng(31.80704, 34.65833
-//                        ),//Top right
-//                        new LatLng(31.80701, 34.6583
-//                        ),//Botom right
-//                        new LatLng(31.80704, 34.65825
-//                        ))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room166A.setTag("Room 166A");
-//
-//        Polygon room167= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80697, 34.65834),//Top left
-//                        new LatLng(31.80694, 34.65839),//Top right
-//                        new LatLng(31.80689, 34.65836),//Botom right
-//                        new LatLng(31.80692, 34.65831))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room167.setTag("Room 167");
-//
-//        Polygon room168= googleMap.addPolygon(new PolygonOptions()
-//                .add(new LatLng(31.80691, 34.65844),//Top left
-//                        new LatLng(31.80689, 34.65849),//Top right
-//                        new LatLng(31.80685, 34.65846),//Botom right
-//                        new LatLng(31.80687, 34.65842))//Bottom left
-//                .strokeColor(Color.BLACK)
-//                .fillColor(0x7F00FF00)
-//                .clickable(true));
-//        room168.setTag("Room 168");
-//
-//
-//
-//
-//
+
+        Polygon room161 = googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80728, 34.65785),//Top left
+                        new LatLng(31.80725, 34.6579),//Top right
+                        new LatLng(31.80722, 34.65787),//Botom right
+                        new LatLng(31.80724, 34.65782))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room161.setTag("Room 161");
+
+
+
+
+
+
+
+        Polygon room162 = googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80721, 34.65787),//Top left
+                        new LatLng(31.80719, 34.65793),//Top right
+                        new LatLng(31.80714, 34.65789),//Botom right
+                        new LatLng(31.80717, 34.65784))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room162.setTag("Room 162");
+
+        Polygon room163= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80719, 34.65793),//Top left
+                        new LatLng(31.80716, 34.65799),//Top right
+                        new LatLng(31.80711, 34.65796),//Botom right
+                        new LatLng(31.80714, 34.6579))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room163.setTag("Room 163");
+
+        Polygon room164= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80713, 34.65802),//Top left
+                        new LatLng(31.80711, 34.65807),//Top right
+                        new LatLng(31.80707, 34.65804),//Botom right
+                        new LatLng(31.80709, 34.658))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room164.setTag("Room 164");
+
+        Polygon room165= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80715, 34.6581),//Top left
+                        new LatLng(31.80712, 34.65816),//Top right
+                        new LatLng(31.80709, 34.65814),//Botom right
+                        new LatLng(31.80712, 34.65807))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room165.setTag("Room 165");
+
+        Polygon room166B= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80706, 34.65817),//Top left
+                        new LatLng(31.80702, 34.65822),//Top right
+                        new LatLng(31.80699, 34.65819),//Botom right
+                        new LatLng(31.80702, 34.65814))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room166B.setTag("Room 166B");
+
+        Polygon room166C= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80701, 34.65825),//Top left
+                        new LatLng(31.80698, 34.6583),//Top right
+                        new LatLng(31.80695, 34.65828),//Botom right
+                        new LatLng(31.80697, 34.65823))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room166C.setTag("Room 166C");
+
+        Polygon room166A= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80707, 34.65827),//Top left
+                        new LatLng(31.80704, 34.65833),//Top right
+                        new LatLng(31.80701, 34.65831),//Botom right
+                        new LatLng(31.80704, 34.65825))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room166A.setTag("Room 166A");
+
+        Polygon room167= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80697, 34.65834),//Top left
+                        new LatLng(31.80694, 34.65839),//Top right
+                        new LatLng(31.80689, 34.65836),//Botom right
+                        new LatLng(31.80692, 34.65831))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room167.setTag("Room 167");
+
+        Polygon room168= googleMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(31.80691, 34.65844),//Top left
+                        new LatLng(31.80689, 34.65849),//Top right
+                        new LatLng(31.80685, 34.65846),//Botom right
+                        new LatLng(31.80687, 34.65842))//Bottom left
+                .strokeColor(Color.BLACK)
+                .fillColor(0x7F00FF00)
+                .clickable(true));
+        room168.setTag("Room 168");
+
+
+
+
+
 
 
 
